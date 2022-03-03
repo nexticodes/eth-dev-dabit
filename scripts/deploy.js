6bc7b058e643542fe38c11bbba2bdfba4440772f
Ernest Bruno <erndobruno@gmail.com>
2022-03-02 21:29:58 -0800

    Finish Complete Guid to Full Stack Ethereum Development Nader Dabit.

---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+
+const hre = require("hardhat");
+
+async function main() {
+
+  // We get the contract to deploy
+  const Greeter = await hre.ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, Hardhat!");
+
+  await greeter.deployed();
+
+  console.log("Greeter deployed to:", greeter.address);
+
+  const Token = await hre.ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+  await token.deployed();
+
+  console.log("Token deployed to:", token.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
